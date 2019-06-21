72b9c0d85e2b01b7ea87edcacabd061ae052a0b3
guanrong.yang <guanrong.yang@bees360.com>
2019-06-21 11:28:57 +0800

    add protobuf maven settings

---
+++ spring-protobuf-validation/protobufvalidation/README.md
@@ -0,0 +1,1 @@
+项目导入之后，请执行“Maven Update”以将proto文件生成java类。
---
+++ spring-protobuf-validation/protobufvalidation/src/main/java/protobufvalidation/Main.java
@@ -0,0 +1,10 @@
+package protobufvalidation;
+
+import cn.donespeak.exchange.Validation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(Validation.class);
+	}
+}
