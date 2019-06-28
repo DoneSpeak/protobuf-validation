be50eceb5d72ea3807fd51e2a88c7671ed57b3ec
guanrong.yang <guanrong.yang@bees360.com>
2019-06-28 09:16:25 +0800

    try to add a new type of validation contraint

--- spring-protobuf-validation/protobufvalidation/src/test/java/cn/donespeak/protobufvalidation/GeneratedMessageV3Test.java
+++ spring-protobuf-validation/protobufvalidation/src/test/java/cn/donespeak/protobufvalidation/GeneratedMessageV3Test.java
@@ -1,11 +1,13 @@
  package cn.donespeak.protobufvalidation;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.GeneratedMessageV3;
 
+import cn.donespeak.exchange.NewValidation;
 import cn.donespeak.exchange.UserProto;
 
 /**
@@ -15,9 +17,13 @@ import cn.donespeak.exchange.UserProto;
 public class GeneratedMessageV3Test {
 
     public static void main(String[] args) {
-        UserProto.UserRequest messageV3 = UserProto.UserRequest.newBuilder()
-            .setEmail("dfdf@e.com").setName(null).build();
+        UserProto.UserRequest userRequest = UserProto.UserRequest.newBuilder()
+            .setEmail("dfdf@e.com").setName("").build();
        
+        NewValidation.NewData newData = NewValidation.NewData.newBuilder().addAllNumbers(Arrays.asList(1, 4, 5)).build();
+        
+        GeneratedMessageV3 messageV3 = newData;
+        
         for (Descriptors.FieldDescriptor fieldDescriptor : messageV3.getDescriptorForType().getFields()) {
             
             System.out.println("field protoName: " + fieldDescriptor.getFile().getFullName());
@@ -36,6 +42,8 @@ public class GeneratedMessageV3Test {
             System.out.println("descriptor javaType: " + fieldDescriptor.getJavaType() + ", class: " + fieldDescriptor.getJavaType().getClass());
             // ENUM
             System.out.println("field type: " + fieldDescriptor.getType());
+            
+            System.out.println("field type: " + fieldDescriptor.getOptions());
             
             System.out.println("=======================================");
         }
